129d7f41648150640ba4cbfe712ac9973fb70699
Mohamed Idris <idrismoha2@ecs.vuw.ac.nz>
2019-07-13 14:51:49 +1200

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,23 @@
 
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-import 'package:u_up/pages/login/signIn.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
+   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: SignInPage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        ),
+      ),
     );
   }
 }
