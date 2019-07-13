7ace1878cc97f1180c0bc810fb2f0b9eedeee733
Mohamed Idris <idrismoha2@ecs.vuw.ac.nz>
2019-07-13 18:28:53 +1200

    added anonymous user

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -4,13 +4,12 @@
 @import UIKit;
 @import Firebase;
 
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
 didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [FIRApp configure];
-    return YES;
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+    return [super application:application didFinishLaunchingWithOptions:launchOptions]; // YES;
 }
-
 @end
