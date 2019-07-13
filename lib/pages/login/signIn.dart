b39763a9952b574ca7ad3b66cae5a15b2e7d9266
Mohamed Idris <idrismoha2@ecs.vuw.ac.nz>
2019-07-13 14:15:47 +1200

    added firebase plugin

--- lib/pages/login/signIn.dart
+++ lib/pages/login/signIn.dart
@@ -4,17 +4,21 @@
 
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-import 'package:u_up/pages/login/signIn.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: SignInPage(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        ),
+      ),
     );
   }
 }
